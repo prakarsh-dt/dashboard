d056712d080a731c59139f5ce4b545f82df9c2d6
Rashmi Rai <rashmi.rai@devtron.ai>
2021-02-16 18:12:17 +0530

    env.development removed from .gitignore

--- src/components/clusterAndNodes/ClusterList.tsx
+++ src/components/clusterAndNodes/ClusterList.tsx
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
+import { ViewType } from '../../config';
+import { ReactComponent as Add } from '../../assets/icons/ic-add.svg';
 
-export class ClusterList extends Component<{}, {}> {
+interface ClusterListState {
 
-    constructor(props){
+}
+export class ClusterList extends Component<{}, any> {
+
+    constructor(props) {
         super(props);
-        this.state={
-
+        this.state = {
+            statusCode: 0,
+            view: ViewType.LOADING,
+            searchObject: { label: "application", value: 'appName' },
+            searchObjectValue: "",
+            searchApplied: false,
+            filters: {
+                environments: [],
+                clusters: [],
+                severity: [],
+            },
+            filtersApplied: {
+                environments: [],
+                clusters: [],
+                severity: [],
+            },
+            showAddFilter: false,
         }
+    }
+
+    toggleAddFilter() {
+        this.setState({ showAddFilter: !this.state.showAddFilter });
+    }
+
+    renderAddFilter() {
+        return <input type="text" />
     }
 
     render() {
         return <div>
             <h1>Cluster & Nodes</h1>
+            <button type="button" onClick={this.toggleAddFilter}>
+                <Add className="icon-dim-20" />
+                Add Filter
+            </button>
         </div>
     }
 }
